318b71aac8a7c36f5e3348023acdc6764b862f15
TejasThombare20 <img_2020066@iiitm.ac.in>
2023-12-29 04:07:49 +0530

    fixing deplyment error

--- src/app/api/contribution/[username]/route.ts
+++ src/app/api/contribution/[username]/route.ts
@@ -26,7 +26,7 @@ export async function GET(
 
       return NextResponse.json({ data }, { status: 200 });
     } catch (error) {
-      return NextResponse.json(error, { status: 400 });
+      return NextResponse.json(error, { status: 500 });
     }
   }
 }
